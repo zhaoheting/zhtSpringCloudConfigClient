a57fa76f2e6192651cd09466741e5178f2c64c91
Heting.Zhao <heting.zhao@openjawtech.com>
2019-09-09 17:41:10 +0800

    self learning

--- src/main/java/com/example/zhtSpringCloudConfigClient/config/ConfigService.java
+++ src/main/java/com/example/zhtSpringCloudConfigClient/config/ConfigService.java
@@ -1,5 +1,7 @@
 package com.example.zhtSpringCloudConfigClient.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
@@ -12,23 +14,11 @@ public class ConfigService {
 
     //The property from designated file will be covered by the property
     // from application.properties with the same name as it.
+    @Getter
+    @Setter
     private String name;
 
+    @Getter
+    @Setter
     private Long age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getAge() {
-        return age;
-    }
-
-    public void setAge(Long age) {
-        this.age = age;
-    }
 }
